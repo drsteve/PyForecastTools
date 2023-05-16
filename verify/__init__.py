cba92993413e8da286d7ed0beb49e566303ba890
Steve Morley <steven.k.morley@gmail.com>
2023-05-16 14:29:46 -0600

    Update attribution/copyright across file headers

--- verify/__init__.py
+++ verify/__init__.py
@@ -12,10 +12,8 @@ metric calculations.
 
 Author: Steve Morley
 Institution: Los Alamos National Laboratory
-Contact: smorley@lanl.gov
-Los Alamos National Laboratory
 
-Copyright (c) 2017, Los Alamos National Security, LLC
+Copyright (c) 2017, Triad National Security, LLC
 All rights reserved.
 """
 from .metrics import *
